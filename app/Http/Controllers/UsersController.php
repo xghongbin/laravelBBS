cf79974a501423989c253143869a9449be9ae681
echoso <xghongbin@163.com>
2019-06-14 20:07:59 +0800

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,14 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
+
 
 class UsersController extends Controller
 {
 
+    //  展示个人页面
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    //  编辑个人页面
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    //  更新个人资料
+    public function update(UserRequest $request , User $user){
+
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }

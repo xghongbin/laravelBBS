482e2ca4b1e7fe3d9c6051033a890a18b67e7343
echoso <xghongbin@163.com>
2019-06-14 21:14:11 +0800

    上传头像功能

--- app/helpers.php
+++ app/helpers.php
@@ -5,3 +5,11 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+function mkdirs($dir, $mode = 0777)
+{
+    if (is_dir($dir) || @mkdir($dir, $mode)) return TRUE;
+    if (!mkdirs(dirname($dir), $mode)) return FALSE;
+
+    return @mkdir($dir, $mode);
+}

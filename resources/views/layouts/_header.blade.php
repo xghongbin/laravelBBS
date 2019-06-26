ed96c53b6dd51aeda16216dcee9f39018d6931c1
echoso <xghongbin@163.com>
2019-06-26 23:57:50 +0800

    用户创建话题

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -37,6 +37,12 @@
                     <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
                     <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
                 @else
+                    <li class="nav-item">
+                        <a class="nav-link mt-1 mr-3 font-weight-bold" href="{{ route('topics.create') }}">
+                            <i class="fa fa-plus"></i>
+                        </a>
+                    </li>
+
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -48,7 +54,7 @@
                             <a class="dropdown-item" href="{{  route('users.show', Auth::id()) }}">个人中心</a>
                             <a class="dropdown-item" href="{{  route('users.edit', Auth::id()) }}">编辑资料</a>
                             <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" id="logout" href="#">
+                            <a class="dropdown-item" id="logout" href="">
                                 <form action="{{ route('logout') }}" method="POST" onsubmit="return confirm('您确定要退出吗？');">
                                     {{ csrf_field() }}
                                     <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
